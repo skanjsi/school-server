ec8f94dd34d8ba08ab5cb34d74d0dace5143d718
Hyunwoo Lee <skanjsi@gmail.com>
2020-08-14 21:36:09 +0900

    20200814 school

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../utils/mysql');
+const crypto = require('crypto'); // hash
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 /* GET users listing. */
 
@@ -26,18 +29,63 @@ router.get('/', async (req, res, next) => {
   }
 });
 
+//sign up api
 router.post('/', async (req, res, next) => {
   try {
     const name = req.body.name;
     const money = req.body.money; //using body
     // const name = req.query.name;
     // const money = req.query.money; //using url(it is not secure)
+    const email = req.body.email;
+    const pwd = req.body.pwd;
+
+    const saltByte = await crypto.randomBytes(64);
+    const salt = saltByte.toString('base64'); // make random string(64)
+    const hashedPwdByte = crypto.pbkdf2Sync(pwd, salt, 100000, 64, 'SHA512'); // 'SHA512' is algorithm
+    const hashedPwd = hashedPwdByte.toString('base64');
+
     const connection = await pool.getConnection();
-    await connection.query('INSERT INTO USER(name, money) VALUES(?, ?)', [
-      name,
-      money,
-    ]);
+    await connection.query(
+      'INSERT INTO USER(name, money, email, hashed_pwd, salt) VALUES(?, ?, ?, ?, ?)',
+      [name, money, email, hashedPwd, salt]
+    );
     res.json({ status: 201, msg: 'Success!' });
+  } catch (err) {
+    console.log(err);
+    res.json({ status: 500, msg: 'Error!' });
+  }
+});
+
+//login api
+router.post('/login', async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    const pwd = req.body.pwd;
+
+    //check email
+    const connection = await pool.getConnection();
+    const [users] = await connection.query(
+      'SELECT * FROM USER WHERE email = ?',
+      [email]
+    );
+    if (users.length === 0) {
+      return res.json({ status: 404, msg: 'not a registered email' });
+    }
+    const user = users[0];
+    const loginHashedPwdByte = crypto.pbkdf2Sync(
+      pwd,
+      user.salt,
+      100000,
+      64,
+      'SHA512'
+    );
+    const loginHashedPwd = loginHashedPwdByte.toString('base64');
+    if (loginHashedPwd !== user.hashed_pwd) {
+      return res.json({ status: 401, msg: 'password is not correct' });
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+    res.json({ status: 200, token: token });
   } catch (err) {
     console.log(err);
     res.json({ status: 500, msg: 'Error!' });
@@ -46,13 +94,26 @@ router.post('/', async (req, res, next) => {
 
 router.get('/:id/profile', async (req, res, next) => {
   try {
-    const id = req.params.id;
-    const connection = await pool.getConnection();
-    const [results] = await connection.query(
-      'SELECT * FROM USER WHERE id = ?',
-      [id]
-    );
-    res.json({ status: 200, arr: results });
+    try {
+      const token = req.headers.authorization;
+      const payload = jwt.verify(token, process.env.JWT_SECRET);
+      const userId = payload.id;
+
+      const id = req.params.id;
+
+      if (userId !== Number(id)) {
+        return res.json({ status: 403, msg: 'you are not authorized' });
+      }
+
+      const connection = await pool.getConnection();
+      const [
+        results,
+      ] = await connection.query('SELECT * FROM USER WHERE id = ?', [id]);
+      res.json({ status: 200, arr: results });
+    } catch (err) {
+      console.log(err);
+      res.json({ status: 401, msg: 'incorrect token' });
+    }
   } catch (err) {
     console.log(err);
     res.json({ status: 500, msg: 'Error!' });
